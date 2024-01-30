e773420801d22d804dc4868ec87b63166dbbafad
Nishan123 <girinishan1234567890@gmail.com>
2024-01-30 21:46:17 +0545

    uploded project files

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("Stock Panda")
+root.geometry("1100x700")
+root.maxsize(height=700,width=1100)
+root.minsize(height=700,width=1100)
+
+
+root.mainloop()
+
