380820d264e0642a9ff6b9b416e9cd67b188b030
Nishan123 <girinishan1234567890@gmail.com>
2024-01-31 21:28:35 +0545

    login screen UI

--- main.py
+++ main.py
@@ -9,30 +9,31 @@ root.geometry("1100x700")
 root.maxsize(height=700, width=1100)
 root.minsize(height=700, width=1100)
 
+container = Frame(bg="#D9D9D9", height=700, width=450)
+container.pack_propagate(False)
+container.pack(side=RIGHT)
+
 # Custom text styles
 customButtonFont = font.Font(size=10, weight="bold")
 customTitleFont = font.Font(size=20, weight="bold")
 
 # to display text logo
-textlogo = PhotoImage(file="textlogo.png")
+textlogo = PhotoImage(file="assets/textlogo.png")
 logoDispaly = Label(root, image=textlogo, bg="#8A908B")
 logoDispaly.place(x=30, y=20)
 
 # for hero text
-Label(root, text="Stock Panda", bg="#8A908B", font=("Arial", 30, "bold")).place(x=30, y=290)
-Label(root, text="Some Description about the software", bg="#8A908B", font=("Arial", 20, "bold")).place(x=30, y=340)
+Label(root, text="Stock Panda", bg="#8A908B", font=("Arial", 30, "bold"),fg="white").place(x=30, y=290)
+Label(root, text="Some Description about the software", bg="#8A908B", font=("Arial", 20, "bold"),fg="white").place(x=30, y=340)
 
 
-
-container = Frame(bg="#D9D9D9", height=700, width=450)
-container.pack_propagate(False)
-container.pack(side=RIGHT)
 
 h1 = Label(container, text="Get Started", bg="#D9D9D9", font=customTitleFont)
-h1.place(x=150, y=250)
+h1.place(x=150, y=260)
 
+# to place button horizantally
 buttonContainer = Frame(container, height=5, width=400, bg="#D9D9D9")
-buttonContainer.place(x=60, y=300)
+buttonContainer.place(x=60, y=320)
 
 login = Button(buttonContainer, text="Log in", height=2, width=20, border=0, bg="#FF5252", fg="white",
                font=customButtonFont)
---
+++ screens/login_screen.py
@@ -0,0 +1,59 @@
+from tkinter import *
+from tkinter import font
+
+root = Tk()
+root.title("Stock Panda")
+root.config(bg="#8A908B")
+root.geometry("1100x700")
+root.maxsize(height=700, width=1100)
+root.minsize(height=700, width=1100)
+
+# Custom text styles
+customButtonFont = font.Font(size=16, weight="bold")
+customTitleFont = font.Font(size=20, weight="bold")
+
+# for hero text
+Label(root, text="Stock Panda", bg="#8A908B", font=("Arial", 30, "bold"),fg="white").place(x=30, y=290)
+Label(root, text="Some Description about the software", bg="#8A908B", font=("Arial", 20, "bold"),fg="white").place(x=30, y=340)
+
+
+# to display text logo
+textlogo = PhotoImage(file="assets/textlogo.png")
+logoDispaly = Label(root, image=textlogo, bg="#8A908B")
+logoDispaly.place(x=30, y=20)
+
+container = Frame(bg="#D9D9D9", height=700, width=450)
+container.pack_propagate(False)
+container.pack(side=RIGHT)
+
+# to display image logo
+imageLogo = PhotoImage(file="assets/imagelogo.png")
+logoDispaly = Label(container, image=imageLogo, bg="#D9D9D9")
+logoDispaly.place(x=150, y=50)
+
+# Welcome text
+h1 = Label(container, text="Welcome Back", bg="#D9D9D9", font=customTitleFont)
+h1.place(x=140, y=230)
+
+# for text fields
+phone = Entry(container,width=27,font=(10))
+phone.place(x=90,y=320,height=38)
+
+password = Entry(container,width=27,font=(10))
+password.place(x=90,y=370,height=38)
+
+
+# for login button
+login = Button(container, text="Log in", height=1, width=22, border=0, bg="#FF5252", fg="white",font=customButtonFont)
+login.place(x=96,y=540)
+
+# for forgot password button
+forgotPass = Button(container,text="Forgot password?",border=0,fg="blue",bg="#D9D9D9")
+forgotPass.place(x=290,y=415)
+
+# create new account button
+signUp = Button(container,text="Create an account",border=0,fg="blue",bg="#D9D9D9",font=1)
+signUp.place(x=160,y=650)
+
+
+root.mainloop()
